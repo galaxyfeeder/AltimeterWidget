b35c00bf4729152f1a20409c3e85e50134cd9814
Gabriel Esteban <gabriel@tomorrowdev.com>
2014-11-30 16:30:33 +0100

    Dummy Files

--- AltimeterWidget/src/com/tomorrowdev/altimeterwidget/WidgetProvider.java
+++ AltimeterWidget/src/com/tomorrowdev/altimeterwidget/WidgetProvider.java
@@ -4,16 +4,20 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 
 public class WidgetProvider extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-		// Build the intent to call the service
+		SensorManager mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+		if(mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) == null){
+			//TODO show alert
+		}
+		
 	    Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
-
-	    // Update the widgets via the service
 	    context.startService(intent);
 	}
 }
